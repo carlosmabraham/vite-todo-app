d07a8dcf842ebbaad6e15781a32c11209b9112ce
Carlos Meza <carlosmabraham@gmail.com>
2023-06-07 08:55:26 -0700

    update

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,12 +1,27 @@
 import html from './app.html?raw'
+import todoStore from '../store/todo.store';
+import { renderTodos } from './use-cases';
+
+const ElementIDs = {
+    TodoList: '.todo-list',
+}
+
 /**
  * 
  * @param {String} elementId pasamos el div donde queremos que se renderise nuestra aplicacion
  */
 export const App = ( elementId ) => {
+
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+        renderTodos( ElementIDs.TodoList, todos );
+    }
+
+
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector( elementId ).append( app );
+        displayTodos();
     })();
 }
---
+++ src/todos/use-cases/render-todos.js
@@ -0,0 +1,10 @@
+import { Todo } from '../models/todo.model'
+
+/**
+ * 
+ * @param {String} elementId 
+ * @param {Todo} todos 
+ */
+export const renderTodos = ( elementId, todos = [] ) => {
+
+}
